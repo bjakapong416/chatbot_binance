573222c8b4f298126bb3ea6a311e9af57e3f226f
bjakapong416 <boon.jakapong@gmail.com>
2021-06-19 22:13:09 +0700

    update flex-message

--- server.py
+++ server.py
@@ -18,29 +18,35 @@ import matplotlib.pyplot as plt
 
 
 from flask import Flask, request, abort
+#pip install line-bot-sdk
+from linebot import LineBotApi
+from linebot.models import TextSendMessage, FlexSendMessage
+#from linebot.models import (
+#    TextSendMessage, FlexSendMessage
+#)
+#from linebot.models.template import *
+#from linebot import (
+#    LineBotApi, WebhookHandler
+#)
 import requests
 import json
 import subprocess
-
-
 
 app = Flask(__name__)
 
 binance = ccxt.binance({'enableRateLimit': True})
 markets = binance.load_markets()
 
-
 @app.route('/', methods=['POST','GET'])
 def webhook():
     if request.method == 'POST':
         payload = request.json
         Reply_token = payload['events'][0]['replyToken']
-        print(Reply_token)
+        #print(Reply_token)
         message = payload['events'][0]['message']['text']
         
         message = message.upper()
         cmd1 = f'python getData_binance.py {message}'
-
 
         # Using os.system() method
         os.system(cmd1)
@@ -61,41 +67,35 @@ def webhook():
         fourth_level = maximum_price - difference * 0.618  
         fifth_level = maximum_price - difference * 0.786
 
+        url_picPlot = "https://forexreddit.com/wp-content/uploads/2020/03/wi3b3y9b.jpg"
+
         # if 'bitcoin' in message :
-        if message in markets:     
-            Reply_messasge = f'{message} \rLast price: {last_price} \r\nFibonanci Level \r\n0%: {maximum_price} \r\n23.6%: {first_level} \r\n38.2%%: {second_level} \r\n50.0%: {third_level} \r\n61.8%: {fourth_level} \r\n78.6%: {fifth_level} \r\n100%: {minimum_price}'
-            ReplyMessage(Reply_token,Reply_messasge,'2IQWSiweGIvQJAlWFtiElnrJHclALAadSbj9delCu8Iv2DQoWXsHO1/1YAHouQ6BYDlAS5oFz2x3Ui1s9mJswr1qqdBPmiKCLOaB+WvWkSmfEq7h26jiVmVpJIzZTVUBYtKZc6C0f/cvSkkm8lnLYgdB04t89/1O/w1cDnyilFU=')
+        if message in markets:    
+
+            Reply_messasge = '''{"type": "bubble","header": {"type": "box","layout": "vertical","contents": [{"type": "text","text": "%s","color": "#1DB446","size": "xxl","weight": "bold","align": "center"}],"backgroundColor": "#000000"},"body": {"type": "box","layout": "vertical","contents": [{"type": "box","layout": "vertical","contents": [{"type": "box","layout": "horizontal","contents": [{"type": "text","text": "Last Price","color": "#000000","size": "lg","weight": "bold"},{"type": "text","text": "$%s","align": "end"}]},{"type": "separator","color": "#000000"}]},{"type": "box","layout": "horizontal","contents": [{"type": "text","text": "Fibonacci (%%)","size": "lg","weight": "bold"},{"type": "text","text": "($)","weight": "bold","size": "lg","align": "end"}],"borderColor": "#000000","paddingBottom": "sm","paddingTop": "xl"},{"type": "box","layout": "horizontal","contents": [{"type": "text","text": "0","size": "sm","flex": 0},{"type": "text","text": "%s","size": "sm","color": "#111111","align": "end"}],"paddingStart": "xl"},{"type": "box","layout": "horizontal","contents": [{"type": "text","text": "23.6","size": "sm","flex": 0},{"type": "text","text": "%s","size": "sm","color": "#111111","align": "end"}],"paddingStart": "xl"},{"type": "box","layout": "horizontal","contents": [{"type": "text","text": "38.2","size": "sm","flex": 0},{"type": "text","text": "%s","size": "sm","color": "#111111","align": "end"}],"paddingStart": "xl"},{"type": "box","layout": "horizontal","contents": [{"type": "text","text": "50","size": "sm","flex": 0},{"type": "text","text": "%s","size": "sm","color": "#111111","align": "end"}],"paddingStart": "xl"},{"type": "box","layout": "horizontal","contents": [{"type": "text","text": "61.8","size": "sm","flex": 0},{"type": "text","text": "%s","size": "sm","color": "#111111","align": "end"}],"paddingStart": "xl"},{"type": "box","layout": "horizontal","contents": [{"type": "text","text": "78.6","size": "sm","flex": 0},{"type": "text","text": "%s","size": "sm","color": "#111111","align": "end"}],"paddingStart": "xl"},{"type": "box","layout": "horizontal","contents": [{"type": "text","text": "100","size": "sm"},{"type": "text","text": "%s","size": "sm","color": "#000000","align": "end"}],"paddingStart": "xl"}]},"footer": {"type": "box","layout": "vertical","contents": [{"type": "image","url": "%s","size": "full","aspectMode": "fit","aspectRatio": "10:3.65","backgroundColor": "#FFFFFF"},{"type": "button","action": {"type": "uri","uri": "%s","label": "Full Picture"},"style": "link"}],"paddingAll": "none","flex": 0},"styles": {"footer": {"separator": true}}}'''%(message,last_price,maximum_price,first_level,second_level,third_level,fourth_level,fifth_level,minimum_price,url_picPlot,url_picPlot)
+
+            ReplyMessage(Reply_token,Reply_messasge,'2IQWSiweGIvQJAlWFtiElnrJHclALAadSbj9delCu8Iv2DQoWXsHO1/1YAHouQ6BYDlAS5oFz2x3Ui1s9mJswr1qqdBPmiKCLOaB+WvWkSmfEq7h26jiVmVpJIzZTVUBYtKZc6C0f/cvSkkm8lnLYgdB04t89/1O/w1cDnyilFU=',1)
             return request.json, 200
         else:
             Reply_messasge = f'Example: BTC/USDT'
-            ReplyMessage(Reply_token,Reply_messasge,'2IQWSiweGIvQJAlWFtiElnrJHclALAadSbj9delCu8Iv2DQoWXsHO1/1YAHouQ6BYDlAS5oFz2x3Ui1s9mJswr1qqdBPmiKCLOaB+WvWkSmfEq7h26jiVmVpJIzZTVUBYtKZc6C0f/cvSkkm8lnLYgdB04t89/1O/w1cDnyilFU=')
+            ReplyMessage(Reply_token,Reply_messasge,'2IQWSiweGIvQJAlWFtiElnrJHclALAadSbj9delCu8Iv2DQoWXsHO1/1YAHouQ6BYDlAS5oFz2x3Ui1s9mJswr1qqdBPmiKCLOaB+WvWkSmfEq7h26jiVmVpJIzZTVUBYtKZc6C0f/cvSkkm8lnLYgdB04t89/1O/w1cDnyilFU=',0)
             return request.json, 200
     else:
         abort(400)
 
-def ReplyMessage(Reply_token, TextMessage, Line_Acees_Token):
+def ReplyMessage(Reply_token, TextMessage, Line_Acees_Token, status):
+    LineBOT = LineBotApi(Line_Acees_Token)
     LINE_API = 'https://api.line.me/v2/bot/message/reply'
-    
-    Authorization = 'Bearer {}'.format(Line_Acees_Token)
-    print(Authorization)
-    headers = {
-        'Content-Type': 'application/json; charset=UTF-8',
-        'Authorization': Authorization
-    }
 
-    data = {
-        "replyToken":Reply_token,
-        "messages":[{
-            "type":"text",
-            "text":TextMessage
-        }]
-    }
-    data = json.dumps(data)
-    r = requests.post(LINE_API, headers=headers, data=data)
+    if status == 0:
+        Reply_Obj = TextSendMessage(text=TextMessage)
+    else:
+        TextMessage = json.loads(TextMessage)
+        Reply_Obj = FlexSendMessage(alt_text='Crypto Price :)', contents=TextMessage)
+        
+    LineBOT.reply_message(Reply_token, Reply_Obj)
     return 200
+    
 
 if __name__ == '__main__':
-
-
-
     app.run(debug=True)
